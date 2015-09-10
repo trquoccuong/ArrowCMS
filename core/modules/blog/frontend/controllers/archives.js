d95ea16edab980cc359b1218727a45c35124764b
QuocCuong <trquoccuong@gmail.com>
2015-09-10 18:01:48 +0700

    add more data to frontend

--- core/modules/blog/frontend/controllers/archives.js
+++ core/modules/blog/frontend/controllers/archives.js
@@ -6,6 +6,7 @@ let _ = require('lodash'),
 let _module = new FrontModule;
 
 _module.list = function (req, res) {
+
     let month_ = req.params.month || '01';
     let year_ = req.params.year || '2000';
     let page = req.params.page || 1;
@@ -16,18 +17,26 @@ _module.list = function (req, res) {
         'from arr_post as posts left outer join arr_user as users on posts.created_by = users.id WHERE' +
         ' "posts"."type" = \'post\' AND "posts"."published" = 1 AND EXTRACT(MONTH FROM posts.created_at ) = ' + month_ + ' AND EXTRACT(YEAR FROM posts.created_at) = ' + year_ +
         ' ORDER BY posts.id ASC OFFSET ' + (page - 1) * number_item + ' LIMIT ' + __config.pagination.number_item;
+    let sqlCount = 'select count(*) ' +
+        'from arr_post as posts WHERE' +
+        ' "posts"."type" = \'post\' AND "posts"."published" = 1 AND EXTRACT(MONTH FROM posts.created_at ) = ' + month_ + ' AND EXTRACT(YEAR FROM posts.created_at) = ' + year_ ;
 
     __models.sequelize.query(sql)
         .then(function (result) {
-            let totalPage = Math.ceil(result[0].count / number_item);
+
             if (result) {
                 // Render view
-                _module.render(req, res, 'archives', {
-                    posts: result[0],
-                    archives_date: year_ + ' ' + month_,
-                    totalPage: totalPage,
-                    currentPage: page
-                });
+                __models.sequelize.query(sqlCount).then(function (countPost) {
+                    let totalPage = Math.ceil(countPost[0][0].count / number_item) || 1;
+                    _module.render(req, res, 'archives', {
+                        posts: result[0],
+                        archives_date: year_ + ' ' + month_,
+                        totalPage: totalPage,
+                        currentPage: page,
+                        route : '/archives/'+year_+'/'+month_+'/page-{page}'
+                    });
+                })
+
             } else {
                 // Redirect to 404 if post not exist
                 _module.render404(req, res);
